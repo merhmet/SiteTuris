19d2d057740b0b2007eee3cb02949ebaf2569f59
merhmet <84395635+merhmet@users.noreply.github.com>
2023-03-12 19:31:11 +0100

    Add files via upload

--- mp3/12Deneme/script.js
+++ mp3/12Deneme/script.js
@@ -1,274 +1,206 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
-
-const playlist = $(".playlist");
-const player = $(".player");
-const dashboard = $(".dashboard");
-const heading = $(".song_name");
-const cd = $(".cd");
-const cdThumb = $(".cd_thumb");
-const progress = $("#progress");
-const audio = $("#audio");
-const playBtn = $(".btn-toggle-play");
-const nextBtn = $(".btn-next");
-const prevBtn = $(".btn-prev");
-const randomBtn = $(".btn-random");
-const repeatBtn = $(".btn-repeat");
-
-const songs = [
+var songsToAdd = [
   {
-    name: "Vì Mẹ Anh Bắt Chia Tay",
-    singer: "Miu Lê, Karik, Châu Đăng Khoa",
-    path: "https://cdn.muzikmp3indir.com/mp3_files/37f4a8a6d51c9dc2984000387fbb0c36.mp3",
-    image:
-      "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/avatars/e/2/9/8/e2983a18669da0f3e941f159ce892b04.jpg",
+    "name": "Terrain",
+    "artist": "pg.lost",
+    "album": "Key",
+    "url": "https://521dimensions.com/songs/Terrain-pglost.mp3",
+    "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/key.jpg"
   },
   {
-    name: "Tệ thật, Anh nhớ em",
-    singer: "Thanh Hưng",
-    path: "https://cdn.muzikmp3indir.com/mp3_files/37f4a8a6d51c9dc2984000387fbb0c36.mp3",
-    image:
-      "https://avatar-ex-swe.nixcdn.com/song/2022/03/03/0/1/3/6/1646267009685_500.jpg",
+    "name": "Vorel",
+    "artist": "Russian Circles",
+    "album": "Guidance",
+    "url": "https://521dimensions.com/songs/Vorel-RussianCircles.mp3",
+    "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/guidance.jpg"
   },
   {
-    name: "Hai mươi hai",
-    singer: "Amee",
-    path: "https://cdn.muzikmp3indir.com/mp3_files/37f4a8a6d51c9dc2984000387fbb0c36.mp3",
-    image:
-      "https://avatar-ex-swe.nixcdn.com/song/2022/05/24/9/6/e/a/1653363505428_500.jpg",
+    "name": "Intro / Sweet Glory",
+    "artist": "Jimkata",
+    "album": "Die Digital",
+    "url": "https://521dimensions.com/songs/IntroSweetGlory-Jimkata.mp3",
+    "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/die-digital.jpg"
   },
   {
-    name: "Từng thương",
-    singer: "Phan Duy Anh, ACV",
-    path: "https://cdn.muzikmp3indir.com/mp3_files/37f4a8a6d51c9dc2984000387fbb0c36.mp3",
-    image:
-      "https://avatar-ex-swe.nixcdn.com/song/2022/04/19/e/7/3/c/1650360805680_500.jpg",
+    "name": "Offcut #6",
+    "artist": "Little People",
+    "album": "We Are But Hunks of Wood Remixes",
+    "url": "https://521dimensions.com/songs/Offcut6-LittlePeople.mp3",
+    "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-but-hunks-of-wood.jpg"
   },
   {
-    name: "Người Em Cố Đô",
-    singer: "Rum, Đaa",
-    path: "https://cdn.muzikmp3indir.com/mp3_files/37f4a8a6d51c9dc2984000387fbb0c36.mp3",
-    image:
-      "https://avatar-ex-swe.nixcdn.com/song/2021/01/06/1/6/a/7/1609922114277_500.jpg",
+    "name": "Dusk To Dawn",
+    "artist": "Emancipator",
+    "album": "Dusk To Dawn",
+    "url": "https://521dimensions.com/songs/DuskToDawn-Emancipator.mp3",
+    "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/from-dusk-to-dawn.jpg"
   },
   {
-    name: "Ánh Sao Và Bầu Trời",
-    singer: "T.R.I",
-    path: "https://cdn.muzikmp3indir.com/mp3_files/37f4a8a6d51c9dc2984000387fbb0c36.mp3",
-    image:
-      "https://avatar-ex-swe.nixcdn.com/song/2021/09/09/f/c/f/d/1631155238247_500.jpg",
-  },
-  {
-    name: "Yêu Em Mỗi Ngày",
-    singer: "Andiez",
-    path: "https://cdn.muzikmp3indir.com/mp3_files/37f4a8a6d51c9dc2984000387fbb0c36.mp3",
-    image:
-      "https://avatar-ex-swe.nixcdn.com/song/2022/03/11/1/a/0/3/1647001991460_500.jpg",
-  },
+    "name": "Anthem",
+    "artist": "Emancipator",
+    "album": "Soon It Will Be Cold Enough",
+    "url": "https://521dimensions.com/songs/Anthem-Emancipator.mp3",
+    "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/soon-it-will-be-cold-enough.jpg"
+  }
 ];
 
-const app = {
-  currentIndex: 0,
-  isPlaying: false,
-  isRandom: false,
-  isRepeat: false,
-  songs: songs,
-  defineProperties: function () {
-    Object.defineProperty(this, "currentSong", {
-      get: function () {
-        return this.songs[this.currentIndex];
-      },
-    });
-  },
-
-  renderSong: function () {
-    var html = "";
-    this.songs.forEach((song, index) => {
-      html += `
-      <div class="song ${
-        index === this.currentIndex ? "active" : ""
-      }" data-index="${index}">
-          <div
-            class="thumb"
-            style="
-              background-image: url(${song.image});
-            "
-          ></div>
-          <div class="body">
-            <h3 class="title">${song.name}</h3>
-            <p class="author">${song.singer}</p>
-          </div>
-          <div class="option">
-            <i class="fas fa-ellipsis-h"></i>
-          </div>
-        </div>
-
-      `;
-    });
-    playlist.innerHTML = html;
-  },
-
-  handleEvent: function () {
-    // Handle hide CD While Scrolling
-    const cdWidth = cd.offsetWidth;
-    document.onscroll = function () {
-      const scrollTop = document.documentElement.scrollTop || window.scrollY;
-      const newCdWidth = cdWidth - scrollTop;
-
-      cd.style.width = newCdWidth > 0 ? newCdWidth + "px" : 0;
-      cd.style.opacity = newCdWidth / cdWidth;
-    };
-
-    // Handle Play Song...
-    playBtn.addEventListener("click", () => {
-      if (app.isPlaying) {
-        audio.pause();
-      } else {
-        audio.play();
-      }
-
-      audio.onplay = () => {
-        app.isPlaying = true;
-        player.classList.add("playing");
-        cdThumb.style.animation = "rotateCD 10s infinite linear";
-      };
-
-      audio.onpause = () => {
-        app.isPlaying = false;
-        player.classList.remove("playing");
-        cdThumb.style.animationPlayState = "paused";
-      };
-
-      audio.ontimeupdate = () => {
-        if (audio.currentTime)
-          progress.value = (audio.currentTime / audio.duration) * 100;
-      };
-
-      progress.onchange = function (e) {
-        const seekTime = (audio.duration / 100) * e.target.value;
-        audio.currentTime = seekTime;
-      };
-    });
-
-    // Handle Next or Prev Song...
-    nextBtn.onclick = () => {
-      if (app.isRandom) {
-        app.playRandomSong();
-      } else {
-        app.nextSong();
-      }
-      player.classList.add("playing");
-      app.renderSong();
-      app.scrollToActiveSong();
-      audio.play();
-    };
-
-    prevBtn.onclick = () => {
-      if (app.isRandom) {
-        app.playRandomSong();
-      } else {
-        app.prevSong();
-      }
-      player.classList.add("playing");
-      app.renderSong();
-      app.scrollToActiveSong();
-      audio.play();
-    };
-
-    // Handle Random Song...
-    randomBtn.onclick = () => {
-      app.isRandom = !app.isRandom;
-      console.log(app.isRandom);
-      randomBtn.classList.toggle("active", app.isRandom);
-    };
-
-    // Handle Ended Song...
-    audio.onended = () => {
-      if (app.isRepeat) {
-        audio.play();
-      } else {
-        nextBtn.click();
-      }
-    };
-
-    // Handle Repeat Song...
-    repeatBtn.onclick = () => {
-      this.isRepeat = !this.isRepeat;
-      repeatBtn.classList.toggle("active", this.isRepeat);
-    };
-
-    playlist.onclick = function (e) {
-      const songNode = e.target.closest(".song:not(.active)");
-      if (songNode || e.target.closest(".option")) {
-        if (songNode) {
-          app.currentIndex = Number(songNode.dataset.index);
-          app.loadCurrentSong();
-          app.renderSong();
-          if (app.isPlaying) {
-            player.classList.add("playing");
-            audio.play();
-          }
-        } else {
-        }
-      }
-    };
-
-    // Handle Active Song...
-    // heading.onchange = () => {
-    //   $(".song.active").classList.remove("active");
-    //   songs[currentIndex].classList.add("active");
-    // };
-  },
-
-  scrollToActiveSong: () => {
-    setTimeout(
-      () =>
-        $(".song.active").scrollIntoView({
-          behavior: "smooth",
-          block: "nearest",
-        }),
-      300
-    );
-  },
-
-  loadCurrentSong: function () {
-    heading.textContent = this.currentSong.name;
-    cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
-    audio.src = this.currentSong.path;
-  },
-
-  nextSong: function () {
-    this.currentIndex++;
-    if (this.currentIndex == this.songs.length) {
-      this.currentIndex = 0;
+Amplitude.init({
+  "songs": [
+    {
+      "name": "Nightmare Side - 2016.09.08",
+      "artist": "Ardan Radio Bandung",
+      "album": "MPTri.my.iD",
+      "url": "https://archive.org/download/nightmareside/2016.09.08.mp3",
+      "cover_art_url": "http://pictures.my.id/images/Nightmare-Side.jpg"
+    },
+    {
+      "name": "Adek Jilbab Biru (ft. Wandra)",
+      "artist": "Jihan Audy",
+      "album": "MPTri.my.iD",
+      "url": "http://abd.filmbluray.id/c10b0a5",
+      "cover_art_url": "https://photos.my.id/images/ejeC.png"
+    },
+    {
+      "name": "The Gun",
+      "artist": "Lorn",
+      "album": "Ask The Dust",
+      "url": "https://521dimensions.com/songs/08 The Gun.mp3",
+      "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/ask-the-dust.jpg"
+    },
+    {
+      "name": "Anvil",
+      "artist": "Lorn",
+      "album": "Anvil",
+      "url": "https://521dimensions.com/songs/LORN - ANVIL.mp3",
+      "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/anvil.jpg"
+    },
+    {
+      "name": "I Came Running",
+      "artist": "Ancient Astronauts",
+      "album": "We Are to Answer",
+      "url": "https://521dimensions.com/songs/ICameRunning-AncientAstronauts.mp3",
+      "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg"
+    },
+    {
+      "name": "First Snow",
+      "artist": "Emancipator",
+      "album": "Soon It Will Be Cold Enough",
+      "url": "http://abd.filmbluray.id/n160908",
+      "cover_art_url": "http://pictures.my.id/images/Nightmare-Side.jpg"
+    },
+    {
+      "name": "First Snow",
+      "artist": "Emancipator",
+      "album": "Soon It Will Be Cold Enough",
+      "url": "https://521dimensions.com/songs/FirstSnow-Emancipator.mp3",
+      "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/soon-it-will-be-cold-enough.jpg"
     }
-    this.loadCurrentSong();
-  },
+  ]
+});
 
-  prevSong: function () {
-    this.currentIndex--;
-    if (this.currentIndex < 0) {
-      this.currentIndex = this.songs.length - 1;
-    }
-    this.loadCurrentSong();
-  },
 
-  playRandomSong: function () {
-    let newIndex;
-    do {
-      newIndex = Math.floor(Math.random() * this.songs.length);
-    } while (this.currentIndex === newIndex);
-    this.currentIndex = newIndex;
-    console.log(this.currentIndex);
-    this.loadCurrentSong();
-  },
+/*
+  Shows the playlist
+*/
+document.getElementsByClassName('show-playlist')[0].addEventListener('click', function(){
+  document.getElementById('white-player-playlist-container').classList.remove('slide-out-top');
+  document.getElementById('white-player-playlist-container').classList.add('slide-in-top');
+  document.getElementById('white-player-playlist-container').style.display = "block";
+});
 
-  start: function () {
-    this.defineProperties();
-    this.handleEvent();
-    this.loadCurrentSong();
-    this.renderSong();
-  },
-};
+/*
+  Hides the playlist
+*/
+document.getElementsByClassName('close-playlist')[0].addEventListener('click', function(){
+  document.getElementById('white-player-playlist-container').classList.remove('slide-in-top');
+  document.getElementById('white-player-playlist-container').classList.add('slide-out-top');
+  document.getElementById('white-player-playlist-container').style.display = "none";
+});
 
-app.start();
+/*
+  Gets all of the add to playlist buttons so we can
+  add some event listeners to actually add to playlist.
+*/
+var addToPlaylistButtons = document.getElementsByClassName('add-to-playlist-button');
+
+for( var i = 0; i < addToPlaylistButtons.length; i++ ){
+  /*
+    Add an event listener to the add to playlist button.
+  */
+  addToPlaylistButtons[i].addEventListener('click', function(){
+    var songToAddIndex = this.getAttribute('song-to-add');
+
+    /*
+      Adds the song to Amplitude, appends the song to the display,
+      then rebinds all of the AmplitudeJS elements.
+    */
+    var newIndex = Amplitude.addSong( songsToAdd[ songToAddIndex ] );
+    appendToSongDisplay( songsToAdd[ songToAddIndex ], newIndex );
+    Amplitude.bindNewElements();
+
+    /*
+      Removes the container that contained the add to playlist button.
+    */
+    var songToAddRemove = document.querySelector('.song-to-add[song-to-add="'+songToAddIndex+'"]');
+    songToAddRemove.parentNode.removeChild( songToAddRemove );
+  });
+}
+
+/*
+  Appends the song to the display.
+*/
+function appendToSongDisplay( song, index ){
+  /*
+    Grabs the playlist element we will be appending to.
+  */
+  var playlistElement = document.querySelector('.white-player-playlist');
+
+  /*
+    Creates the playlist song element
+  */
+  var playlistSong = document.createElement('div');
+  playlistSong.setAttribute('class', 'white-player-playlist-song amplitude-song-container amplitude-play-pause');
+  playlistSong.setAttribute('data-amplitude-song-index', index);
+
+  /*
+    Creates the playlist song image element
+  */
+  var playlistSongImg = document.createElement('img');
+  playlistSongImg.setAttribute('src', song.cover_art_url);
+
+  /*
+    Creates the playlist song meta element
+  */
+  var playlistSongMeta = document.createElement('div');
+  playlistSongMeta.setAttribute('class', 'playlist-song-meta');
+
+  /*
+    Creates the playlist song name element
+  */
+  var playlistSongName = document.createElement('span');
+  playlistSongName.setAttribute('class', 'playlist-song-name');
+  playlistSongName.innerHTML = song.name;
+
+  /*
+    Creates the playlist song artist album element
+  */
+  var playlistSongArtistAlbum = document.createElement('span');
+  playlistSongArtistAlbum.setAttribute('class', 'playlist-song-artist');
+  playlistSongArtistAlbum.innerHTML = song.artist+' &bull; '+song.album;
+
+  /*
+    Appends the name and artist album to the playlist song meta.
+  */
+  playlistSongMeta.appendChild( playlistSongName );
+  playlistSongMeta.appendChild( playlistSongArtistAlbum );
+
+  /*
+    Appends the song image and meta to the song element
+  */
+  playlistSong.appendChild( playlistSongImg );
+  playlistSong.appendChild( playlistSongMeta );
+
+  /*
+    Appends the song element to the playlist
+  */
+  playlistElement.appendChild( playlistSong );
+}
