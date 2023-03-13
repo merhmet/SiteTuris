18f314239a9c9a1f8441cb534facdfe0bcb368ed
merhmet <84395635+merhmet@users.noreply.github.com>
2023-03-13 03:05:34 +0100

    Add files via upload

--- mp3/16Youtube/script.js
+++ mp3/16Youtube/script.js
@@ -1,93 +1,206 @@
-// Setup Swiper-js 
-// https://swiperjs.com/
-// Params
-var sliderSelector = ".swiper-container",
-  options = {
-    init: false,
-    loop: true,
-    speed: 800,
-    slidesPerView: 2, // or 'auto'
-    // spaceBetween: 10,
-    centeredSlides: true,
-    effect: "coverflow", // 'cube', 'fade', 'coverflow',
-    coverflowEffect: {
-      rotate: 50, // Slide rotate in degrees
-      stretch: 0, // Stretch space between slides (in px)
-      depth: 100, // Depth offset in px (slides translate in Z axis)
-      modifier: 1, // Effect multipler
-      slideShadows: true // Enables slides shadows
-    },
-    grabCursor: true,
-    parallax: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: false
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false
-    },
-    breakpoints: {
-      1023: {
-        slidesPerView: 1,
-        spaceBetween: 0
-      }
-    },
-    // Events
-    on: {
-      imagesReady: function () {
-        this.el.classList.remove("loading");
-      },
-      slideChange: function () {
-        //  Stop Video from Playing on Slide Change
-        videoStop();
-      }
-    }
-  };
-var mySwiper = new Swiper(sliderSelector, options);
-
-// Initialize slider
-mySwiper.init();
-
-//  Video Poster Setup
-
-$(document).on("click", ".js-videoPoster", function (ev) {
-  "use strict";
-  ev.preventDefault();
-  videoStop();
-  var $poster = $(this);
-  var $wrapper = $poster.closest(".js-videoWrapper");
-  videoPlay($wrapper);
+// Code goes here
+(function() {
+  
+var myApp = angular.module('MyYoutubeApp', []);
+myApp.constant('YT_event', {
+	STOP:            0, 
+	PLAY:            1,
+	PAUSE:           2
 });
 
-// play the targeted video (and hide the poster frame)
-function videoPlay($wrapper) {
-  "use strict";
-  var $iframe = $wrapper.find(".js-videoIframe");
-  var src = $iframe.data("src");
-  // hide poster
-  $wrapper.addClass("videoWrapperActive");
-  // add iframe src in, starting the video
-  $iframe.attr("src", src);
-}
+myApp.run(function () {
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+});
 
-// stop the targeted/all videos (and re-instate the poster frames)
-function videoStop($wrapper) {
-  "use strict";
-  // if we're stopping all videos on page
-  if (!$wrapper) {
-    var $wrapper = $(".js-videoWrapper");
-    var $iframe = $(".js-videoIframe");
-    // if we're stopping a particular video
-  } else {
-    var $iframe = $wrapper.find(".js-videoIframe");
-  }
-  // reveal poster
-  $wrapper.removeClass("videoWrapperActive");
-  // remove youtube link, stopping the video from playing in the background
-  $iframe.attr("src", "");
-}
+
+myApp.controller('YoutubeCtl', function($scope, YT_event) {
+
+	var ctrl = this;
+
+	var youtube = {
+		player: null,
+		playerId: "Youtube-Player",
+		videoId: null
+	};
+
+	//initial settings
+	$scope.playerStatus = "undefined"
+
+	$scope.YT_event = YT_event;
+
+	$scope.yt_target = null;
+
+	$scope.yt_ready = false;
+
+	ctrl.modal = {
+		status: 'undefined',
+		player: false
+	};
+	
+	ctrl.getPlayer = function() {
+		return this.modal.player;
+	};
+	ctrl.createPlayer = function($yt_target) { 
+		//console.log('createPlayer', youtube);
+		var player = new YT.Player(youtube.playerId, {
+			height: '100%',
+			width: '100%',
+			videoId: youtube.videoId,
+			playerVars: {
+				autoplay: 0,
+				html5: 1,
+				theme: "light",
+				modesbranding: 0,
+				color: "white",
+				iv_load_policy: 3,
+				showinfo: 1,
+				controls: 1
+			}
+		});
+		return player;
+	}
+
+	$scope.addModal = function(modal) {
+		ctrl.modal = modal;
+		ctrl.modal.status = 'closed'
+	}
+
+	$scope.play = function(v) {
+		//console.log(youtube);
+		ctrl.modal.modal('show');
+		if(v == youtube.yt_target)
+			youtube.player.playVideo();
+		else {
+			youtube.player.loadVideoById(v);
+			youtube.yt_target = v;
+		}
+		ctrl.modal.status = 'opened'
+	}
+
+	$scope.closeModal = function() {
+		//console.log('closing');
+		$scope.stopVideo();
+		ctrl.modal.modal('hide');
+
+	}
+	$scope.stopVideo = function() {
+		//console.log('stop videi');
+		youtube.player.seekTo(0);
+		youtube.player.stopVideo();
+		$scope.yt_ready = false;
+
+	}
+
+	$scope.initPlayer = function($videoId) {
+		youtube.player = ctrl.createPlayer();
+		$scope.yt_ready = true;
+	}
+
+});
+
+myApp.directive('youtubeModal', ['$window', '$compile', 'YT_event', function($window, $compile, YT_event) {
+
+
+  return {
+    restrict: "EA",
+
+    link: function($scope, $el, attrs, $rootScope) {
+
+    	var triggers, triggerClass, activate_trigger, player, modal, modalTpl, modalDims;
+
+		modalDims = getModalDims();
+
+
+		modalTpl = "<div class='modal fade youtube-modal' id='YoutubeModal' tabindex='-1' role='dialog' aria-labelledby='YoutubeModalModalLabel'>"+ 
+			"<div class='modal-iframe' style='width:"+modalDims.width+"px; height: "+modalDims.height+"px; margin: 0 auto;'>"+
+				"<div class='modal-iframe-content'>" +
+					"<div class='responsive-embed'>" +
+						"<div class='' id='Youtube-Player'></div>" +
+					"</div>" + 
+					"<div class='text-center modal-close'>" +
+						"<a type='' class=' no-float btn btn-danger' ng-click='closeModal()' aria-label='Close'><span aria-hidden='true'>Close Video</span></a>" +
+					"</div>" + 	
+				"</div>" +
+			"</div>" +
+		"</div>";
+
+		//function createModal
+    	modal = angular.element(modalTpl);
+    	$el.append($compile(modal)($scope));
+
+    	modal.modal({
+    		show: false,
+    	}).on({
+			'hide.bs.modal': function() { $scope.stopVideo(); }
+		});
+
+    	$scope.addModal(modal);
+
+    	
+		triggerClass = (attrs == "") ? '.youtube-link' : '.' + attrs.youtubeModal;
+		
+		triggers = $el.find(triggerClass);
+		activate_trigger = function($trigger) {
+			var href = $($trigger).attr('href');
+			var yt_target = findYoutubeID(href);
+
+			angular.element($trigger).data('yt_target', yt_target).bind('click', function(e) {
+				$scope.play($(this).data('yt_target'));
+				return false;
+			})
+		}
+
+
+		$window.onYouTubeIframeAPIReady = function() {
+			$scope.initPlayer();
+			angular.forEach(triggers, activate_trigger);
+		};
+
+		
+		$scope.$on(YT_event.STOP, function() {
+			player.seekTo(0);
+			player.stopVideo();
+		});
+
+		$scope.$on(YT_event.PLAY, function() {
+			player.playVideo();
+		});
+
+		$scope.$on(YT_event.PAUSE, function() {
+			player.pauseVideo();
+		});
+
+		function findYoutubeID(href) {
+			var match = href.match(/[http(?:s?)?:]?\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/);
+			
+			if(!match || !match.length)
+				return false;
+
+			return match[1];
+		};
+
+
+	    function getModalDims() {
+
+			var w = 0, maxW = 0, winH = $window.innerHeight, winW = $window.innerWidth;
+
+			maxW = .8 * winW;
+			w = (winH / 3) * 4;
+
+			if(w > maxW)
+				w = maxW;
+
+			return {
+				width: Math.round(w),
+				height: Math.round(winH) - 100
+			}
+		};
+
+
+    }
+  };
+}]);
+})();
