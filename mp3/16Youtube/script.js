38ca21d6cfe933f13c77a6bbbcc83642f85a4f36
merhmet <84395635+merhmet@users.noreply.github.com>
2023-03-13 03:49:03 +0100

    Add files via upload

--- mp3/16Youtube/script.js
+++ mp3/16Youtube/script.js
@@ -1,206 +1,115 @@
-// Code goes here
-(function() {
-  
-var myApp = angular.module('MyYoutubeApp', []);
-myApp.constant('YT_event', {
-	STOP:            0, 
-	PLAY:            1,
-	PAUSE:           2
-});
+const load = (function () {
+    "use strict";
 
-myApp.run(function () {
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-});
-
-
-myApp.controller('YoutubeCtl', function($scope, YT_event) {
-
-	var ctrl = this;
-
-	var youtube = {
-		player: null,
-		playerId: "Youtube-Player",
-		videoId: null
-	};
-
-	//initial settings
-	$scope.playerStatus = "undefined"
-
-	$scope.YT_event = YT_event;
-
-	$scope.yt_target = null;
-
-	$scope.yt_ready = false;
-
-	ctrl.modal = {
-		status: 'undefined',
-		player: false
-	};
-	
-	ctrl.getPlayer = function() {
-		return this.modal.player;
-	};
-	ctrl.createPlayer = function($yt_target) { 
-		//console.log('createPlayer', youtube);
-		var player = new YT.Player(youtube.playerId, {
-			height: '100%',
-			width: '100%',
-			videoId: youtube.videoId,
-			playerVars: {
-				autoplay: 0,
-				html5: 1,
-				theme: "light",
-				modesbranding: 0,
-				color: "white",
-				iv_load_policy: 3,
-				showinfo: 1,
-				controls: 1
-			}
-		});
-		return player;
-	}
-
-	$scope.addModal = function(modal) {
-		ctrl.modal = modal;
-		ctrl.modal.status = 'closed'
-	}
-
-	$scope.play = function(v) {
-		//console.log(youtube);
-		ctrl.modal.modal('show');
-		if(v == youtube.yt_target)
-			youtube.player.playVideo();
-		else {
-			youtube.player.loadVideoById(v);
-			youtube.yt_target = v;
-		}
-		ctrl.modal.status = 'opened'
-	}
-
-	$scope.closeModal = function() {
-		//console.log('closing');
-		$scope.stopVideo();
-		ctrl.modal.modal('hide');
-
-	}
-	$scope.stopVideo = function() {
-		//console.log('stop videi');
-		youtube.player.seekTo(0);
-		youtube.player.stopVideo();
-		$scope.yt_ready = false;
-
-	}
-
-	$scope.initPlayer = function($videoId) {
-		youtube.player = ctrl.createPlayer();
-		$scope.yt_ready = true;
-	}
-
-});
-
-myApp.directive('youtubeModal', ['$window', '$compile', 'YT_event', function($window, $compile, YT_event) {
-
-
-  return {
-    restrict: "EA",
-
-    link: function($scope, $el, attrs, $rootScope) {
-
-    	var triggers, triggerClass, activate_trigger, player, modal, modalTpl, modalDims;
-
-		modalDims = getModalDims();
-
-
-		modalTpl = "<div class='modal fade youtube-modal' id='YoutubeModal' tabindex='-1' role='dialog' aria-labelledby='YoutubeModalModalLabel'>"+ 
-			"<div class='modal-iframe' style='width:"+modalDims.width+"px; height: "+modalDims.height+"px; margin: 0 auto;'>"+
-				"<div class='modal-iframe-content'>" +
-					"<div class='responsive-embed'>" +
-						"<div class='' id='Youtube-Player'></div>" +
-					"</div>" + 
-					"<div class='text-center modal-close'>" +
-						"<a type='' class=' no-float btn btn-danger' ng-click='closeModal()' aria-label='Close'><span aria-hidden='true'>Close Video</span></a>" +
-					"</div>" + 	
-				"</div>" +
-			"</div>" +
-		"</div>";
-
-		//function createModal
-    	modal = angular.element(modalTpl);
-    	$el.append($compile(modal)($scope));
-
-    	modal.modal({
-    		show: false,
-    	}).on({
-			'hide.bs.modal': function() { $scope.stopVideo(); }
-		});
-
-    	$scope.addModal(modal);
-
-    	
-		triggerClass = (attrs == "") ? '.youtube-link' : '.' + attrs.youtubeModal;
-		
-		triggers = $el.find(triggerClass);
-		activate_trigger = function($trigger) {
-			var href = $($trigger).attr('href');
-			var yt_target = findYoutubeID(href);
-
-			angular.element($trigger).data('yt_target', yt_target).bind('click', function(e) {
-				$scope.play($(this).data('yt_target'));
-				return false;
-			})
-		}
-
-
-		$window.onYouTubeIframeAPIReady = function() {
-			$scope.initPlayer();
-			angular.forEach(triggers, activate_trigger);
-		};
-
-		
-		$scope.$on(YT_event.STOP, function() {
-			player.seekTo(0);
-			player.stopVideo();
-		});
-
-		$scope.$on(YT_event.PLAY, function() {
-			player.playVideo();
-		});
-
-		$scope.$on(YT_event.PAUSE, function() {
-			player.pauseVideo();
-		});
-
-		function findYoutubeID(href) {
-			var match = href.match(/[http(?:s?)?:]?\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/);
-			
-			if(!match || !match.length)
-				return false;
-
-			return match[1];
-		};
-
-
-	    function getModalDims() {
-
-			var w = 0, maxW = 0, winH = $window.innerHeight, winW = $window.innerWidth;
-
-			maxW = .8 * winW;
-			w = (winH / 3) * 4;
-
-			if(w > maxW)
-				w = maxW;
-
-			return {
-				width: Math.round(w),
-				height: Math.round(winH) - 100
-			}
-		};
-
-
+    function _load(tag) {
+        return function (url) {
+            return new Promise(function (resolve) {
+                const element = document.createElement(tag);
+                const parent = "body";
+                const attr = "src";
+                element.onload = function () {
+                    resolve(url);
+                };
+                element[attr] = url;
+                document[parent].appendChild(element);
+            });
+        };
     }
-  };
-}]);
-})();
+    return {
+        js: _load("script")
+    };
+}());
+(function manageCover() {
+    "use strict";
+
+    function hide(el) {
+        el.classList.add("hide");
+    }
+
+    function coverClickHandler(evt) {
+        const cover = evt.currentTarget;
+        hide(cover);
+    }
+    const cover = document.querySelector(".jacket");
+    cover.addEventListener("click", coverClickHandler);
+}());
+const videoPlayer = (function makeVideoPlayer() {
+    "use strict";
+
+    function onPlayerReady(event) {
+        const player = event.target;
+        player.setVolume(100); // percent
+    }
+    let hasShuffled = false;
+
+    function onPlayerStateChange(event) {
+        const player = event.target;
+        const shufflePlaylist = true;
+
+        if (!hasShuffled) {
+            player.setShuffle(shufflePlaylist);
+            player.playVideoAt(0);
+            hasShuffled = true;
+        }
+    }
+
+    function addVideo(video) {
+
+        const playlist = "PLbMRkajUSrSlmXKzgc5074cZZnsrwAglS,-Xgi_way56U,CHahce95B1g";
+
+        new YT.Player(video, {
+            videoId: video.dataset.id,
+            width: 640,
+            height: 360,
+            host: "https://www.youtube-nocookie.com",
+            playerVars: {
+                autoplay: 1,
+                controls: 1,
+                rel: 0,
+                iv_load_policy: 3,
+                cc_load_policy: 0,
+                fs: 0,
+                disablekb: 1,
+                playlist
+            },
+            events: {
+                "onReady": onPlayerReady,
+                "onStateChange": onPlayerStateChange
+            }
+        });
+    }
+
+    function init(opts) {
+        load.js("https://www.youtube.com/player_api").then(function () {
+            YT.ready(function () {
+                addVideo(opts.video);
+            });
+        });
+    }
+    return {
+        init
+    };
+}());
+
+(function iife() {
+    "use strict";
+
+    function show(el) {
+        el.classList.remove("hide");
+    }
+
+    function initPlayer(wrapper) {
+        videoPlayer.init({
+            video: wrapper.querySelector(".video")
+        });
+    }
+
+    function coverClickHandler(evt) {
+        const wrapper = evt.currentTarget.nextElementSibling;
+        show(wrapper);
+        initPlayer(wrapper);
+    }
+
+    const cover = document.querySelector(".jacket");
+    cover.addEventListener("click", coverClickHandler);
+}());
